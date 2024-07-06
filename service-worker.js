b2af4774cd275c53bdbd5a076578976ce15aad69
rahul raikwar <rr200636@gmail.com>
2024-07-06 21:36:39 +0530

    last commit now fix the popuphtml error

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("Message received in service worker:", request.Message);
-  sendResponse({ Message: "Message received send from service worker" });
-});
