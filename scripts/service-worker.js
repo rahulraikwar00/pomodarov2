8b33ea536974fdb465f6df34b922e9047b6b0049
rahul raikwar <rr200636@gmail.com>
2024-07-10 16:55:21 +0530

    timer with gif tested with option is working with local storage with sync badgeicon

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,32 +1,39 @@
 console.log("service-worker.js is loaded");
 chrome.alarms.clearAll();
+chrome.storage.local.clear();
 
-chrome.storage.local.remove(["timer", "isRunning"]);
+chrome.storage.local.get(["timer", "isRunning"], (result) => {
+  console.log("faltu main running ");
+  chrome.storage.local.set({
+    timer: "timer" in result ? result.timer : 25 * 60,
+    isRunning: "isRunning" in result ? result.isRunning : false,
+  });
+});
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name !== "timer") return;
 
-  chrome.storage.local.get(["timer", "isRunning"], ({ timer, isRunning }) => {
+  chrome.storage.local.get(["timer", "isRunning"], (data) => {
+    let timer = data.timer;
     const minutes = Math.floor(timer / 60);
+    const seconds = timer % 60;
 
-    if (!isRunning) return;
+    chrome.action.setBadgeText({
+      text: `${minutes.toString().padStart(2, "0")}:${seconds
+        .toString()
+        .padStart(2, "0")}`,
+    });
+
+    if (!data.isRunning) return;
 
     timer -= 1;
 
-    if (timer <= 0) {
+    if (timer < 0) {
       timer = 25 * 60;
-      chrome.alarms.clear("timer");
-      console.log("Time's up!");
-      chrome.storage.local.set({ isRunning: false });
+      chrome.storage.local.set({ timer: timer, isRunning: false });
+    } else {
+      chrome.storage.local.set({ timer: timer, isRunning: true });
     }
-
-    console.log("Timer:", timer);
-
-    chrome.action.setBadgeText({
-      text: `${minutes.toString().padStart(2, "0")}M`,
-    });
-
-    chrome.storage.local.set({ timer: timer });
   });
 });
 
@@ -37,10 +44,3 @@ function setAlarm() {
 }
 
 setAlarm();
-
-chrome.storage.local.get(["timer", "isRunning"], (result) => {
-  chrome.storage.local.set({
-    timer: "timer" in result ? result.timer : 25 * 60,
-    isRunning: "isRunning" in result ? result.isRunning : false,
-  });
-});
