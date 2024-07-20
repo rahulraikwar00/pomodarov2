5e6caa69f1368da6c62cec9803cc9108e357aba3
rahul raikwar <rr200636@gmail.com>
2024-07-20 20:06:27 +0530

    delete fatu ki files

--- pomoji-V2/offscreendoc/offscreen.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "playSound") {
-    const audio = new Audio(chrome.runtime.getURL("sound/lofiloop.mp3"));
-    audio.loop = true;
-    audio.volume = 0.5;
-    document.body.appendChild(audio);
-    audio.play();
-  }
-});
